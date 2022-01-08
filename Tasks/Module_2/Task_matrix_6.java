6c2862d59150343dced5275ee7e7635ddb2dfbfa
HutorovaMN <90467323+HutorovaMN@users.noreply.github.com>
2022-01-08 22:08:27 +0300

    Add files via upload

---
+++ Tasks/Module_2/Task_matrix_6.java
@@ -0,0 +1,33 @@
+package Module_2;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_matrix_6 {
+	/*
+	 * Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
+	 */
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of the array that is to be created:");
+		int n = scan.nextInt();
+		matrix(n);
+	}
+
+	public static void matrix(int n) {
+		int[][] array = new int[n][n];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length - i; j++) {
+				array[i][j] = i + 1;
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
